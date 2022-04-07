ae8e2a423c4bfc7cbf36d6f0128e429b0f6ba028
adaj-1 <67992220+adaj-1@users.noreply.github.com>
2022-04-07 17:37:06 -0600

    working get, put, delete, insert test functions

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import * as React from 'react'
 
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
 import Alert from "react-bootstrap/Alert"
 import Axios from 'axios'
@@ -17,6 +17,7 @@ function HomePage() {
   const [numbertest, setNumber] = useState('');
   const [nametest, setName] = useState('');
   const [nameList, setNameList] = useState([]);
+  const [updateName, setUpdate] = useState('');
 
   useEffect(() => {
     Axios.get('http://localhost:3001/api/gettestme').then((response) => {
@@ -25,11 +26,27 @@ function HomePage() {
   }, [])
 
   const submitTest = () => {
-    Axios.post('http://localhost:3001/api/insert',
-    {number_test: numbertest, name_test: nametest}).then(() => {
-      Alert('successful insert');
+    Axios.post('http://localhost:3001/api/insert', {
+      number_test: numbertest,
+      name_test: nametest
     });
+
+    setNameList([...nameList, { number_test: numbertest, name_test: nametest }]);
   };
+
+  const deleteTest = (delete_num) => {
+    Axios.delete(`http://localhost:3001/api/delete/${delete_num}`);
+  };
+
+  const updateTest = (update_num) => {
+    Axios.put("http://localhost:3001/api/update", {
+      number_test: update_num,
+      name_test: updateName
+    });
+
+    setUpdate("");  //resetting updatename to empty
+  };
+
 
   return (
     <Layout pageTitle="The Bookish Calgarian">
@@ -52,7 +69,7 @@ function HomePage() {
           <Row>
             <Col>
               <Form.Label>Number</Form.Label>
-              <Form.Control type="number" placeholder="test" onChange={(e)=> {setNumber(e.target.value)}}/>
+              <Form.Control type="number" placeholder="test" onChange={(e) => { setNumber(e.target.value) }} />
             </Col>
             <Col>
               <Form.Label>Name</Form.Label>
@@ -63,11 +80,20 @@ function HomePage() {
           <Button class="btn btn-primary" onClick={submitTest}>Submit</Button>
 
           {nameList.map((val) => {
-            return <h1> Numbers: {val.number} | Names: {val.name}</h1>
+            return <div>
+              <h2>{val.number}</h2>
+              <p>{val.name}</p>
+              <Form.Label>Update Name</Form.Label>
+              <Form.Control type="text" placeholder="update name" onChange={(e) => { setUpdate(e.target.value) }} />
+              <Button onClick={() => { deleteTest(val.number) }}>Delete</Button>
+              <Button onClick={() => { updateTest(val.number) }}>Update</Button>
+            </div>
           })}
 
         </Form>
       </div >
+
+
 
     </Layout>
   );
